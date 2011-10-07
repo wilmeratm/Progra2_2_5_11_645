acd0a4e85b9f3ef4aab43a737c54a57390caec12
Wilmer Tabora <wilmeratm@unitec.edu>
2011-10-07 08:56:59 -0600

    esta es una prueba para ver si puedo usar esto :P

---
+++ src/progra2_2_5_11_645/Mi_Nombre.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author Wilmer Tabora
+ */
+public class Mi_Nombre {
+
+    public static void main(String[] args ){
+        System.out.println("Wilmer Ariel Tabora");
+    }
+}
